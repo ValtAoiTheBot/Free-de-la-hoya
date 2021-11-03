f077ff43d66751c0d56c0e445a2e5823b9ef3060
ValtAoiTheBot <91861970+ValtAoiTheBot@users.noreply.github.com>
2021-11-03 12:16:39 +0530

    Update zipper.py

--- mrjoker/modules/zipper.py
+++ mrjoker/modules/zipper.py
@@ -47,7 +47,7 @@ async def _(event):
             )
             return
 
-    mone = await event.reply("`⏳️ Wait....🤡`")
+    mone = await event.reply("`⏳️ Wait...`")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     if event.reply_to_msg_id:
@@ -125,11 +125,11 @@ async def _(event):
     if event.is_group:
         if not (await is_register_admin(event.input_chat, event.message.sender_id)):
             await event.reply(
-                " Hi.. You are not admin.. You can't use this command.. But you can use in my pm come my dm baby🤡"
+                " Hi.. You are not admin.. You can't use this command.. But you can use in my pm come my dm baby"
             )
             return
 
-    mone = await event.reply("Processing...🤡")
+    mone = await event.reply("Processing..")
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
     if event.reply_to_msg_id:
